6a96ca9fbb0cd646d1b4f6c9c27acb09d9aac15e
Nikola Ninov <momzzze@abv.bg>
2022-07-21 14:13:25 +0300

    creating  products and product page
    Also adding some dependencies and firebase

--- essence-shop/src/App.js
+++ essence-shop/src/App.js
@@ -2,16 +2,25 @@
 import './App.css';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
+import NavBar from './components/Header/NavBar/NavBar';
 import { Home } from './components/Home/Home';
+import Products from './components/Products/Products';
+import ListComponents from './components/Test/ListComponents';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Footer />
+    <div >
+      <NavBar/>
+      <Products />
+      <main>
+        <ListComponents/>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ essence-shop/src/components/Products/Products.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Grid } from '@material-ui/core';
+
+
+import Product from './Product/Product';
+import useStles from './productsStyle';
+
+const productsDB = [
+
+    { _id: 1, name: 'Picture', description: 'Some Picture', price: '$5', imageUrl: 'https://i.etsystatic.com/13123392/r/il/8c6f2f/3718797167/il_794xN.3718797167_ofvy.jpg' },
+    { _id: 2, name: 'Picture2', description: 'Some Picture 2', price: '$15', imageUrl: 'https://i.etsystatic.com/24098352/r/il/17d99c/2450997257/il_794xN.2450997257_rsbk.jpg' },
+    { _id: 3, name: 'Picture 3', description: 'Some Picture 3', price: '$10', imageUrl: 'https://i.etsystatic.com/34854089/r/il/fff0c6/3824682789/il_794xN.3824682789_j2lr.jpg' },
+
+]
+
+
+const Products = () => {
+    const classes = useStles();
+
+    return (
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Grid container justifyContent="center" spacing={4}>
+                {productsDB.map((product) => (
+                    <Grid item key={product._id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
+    )
+}
+
+export default Products
