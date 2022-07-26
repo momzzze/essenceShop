aa084dd7edff88241926641e58e79c3ed84af69f
Nikola Ninov <momzzze@abv.bg>
2022-07-26 19:10:10 +0300

    link details page for  the exact id

--- essence-shop/src/components/Products/DetailsProdcut/DetailsProduct.js
+++ essence-shop/src/components/Products/DetailsProdcut/DetailsProduct.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
-const DetailsProduct = ({props}) => {
-  const clicked=()=>{
-      console.log(props);
+
+const DetailsProduct = ({ products }) => {
+  const { productId } = useParams()
+  const product = products.find(x => x.id == productId);
+
+  const clicked = () => {
+    console.log(product);
   }
+
+  
   return (
     <>
       <button onClick={clicked}>Props</button>
