6ffb62465b55a6cbb2ce79c6a9adc087b2666ffd
Nikola Ninov <momzzze@abv.bg>
2022-08-07 17:48:20 +0300

    rebuilding some elements in navbar

--- essence-shop/src/components/Header/NavBar/NavBar.js
+++ essence-shop/src/components/Header/NavBar/NavBar.js
@@ -1,17 +1,112 @@
-import React from 'react'
-import { AppBar, Toolbar, IconButton, ListItem, ListItemText, Button, ListItemIcon } from '@material-ui/core';
-import { Badge } from '@mui/material';
-import { ShoppingCart } from '@material-ui/icons';
+import React from 'react';
+import {
+    AppBar, Toolbar, SwipeableDrawer, IconButton, List, ListItem, ListItemText,
+    useScrollTrigger, Menu, MenuItem, makeStyles,
+    Tabs, Tab, Button, useTheme, useMediaQuery, Badge
+} from '@material-ui/core';
+import MenuIcon from "@material-ui/icons/Menu";
+import logo from '../../../assets/logo.png';
+import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import useStyles from './navStyle';
-import logo from '../../../assets/essenceLogo.png';
-import { Stack } from '@mui/material';
+import { ShoppingCart } from '@material-ui/icons';
 import { logout } from '../../Auth/authLogic';
 import { auth } from '../../../lib/init-firebase';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
+
+function ElevationScroll(props) {
+    const { children } = props;
+
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+    });
+
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    });
+}
+
 
 const NavBar = () => {
-    const classes = useStyles()
+    const classes = useStyles();
     const navigate = useNavigate();
+    const theme = useTheme();
+
+    const matches = useMediaQuery(theme.breakpoints.down("md"));
+    const [openDrawer, setOpenDrawer] = useState(false);
+    const [value, setValue] = useState(0);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [openMenu, setOpenMenu] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+
+
+    const navLinkChangeHandler = (e, newValue) => {
+        setValue(newValue);
+    }
+    const handleClick = (e, value) => {
+        setAnchorEl(e.currentTarget);
+        setOpenMenu(true);
+    }
+    const handleClose = (e, value) => {
+        setAnchorEl(null);
+        setOpenMenu(false);
+    }
+    const handleMenuItemClick = (e, i) => {
+        setAnchorEl(null);
+        setOpenMenu(false);
+        setSelectedIndex(i);
+    }
+    const menuOptions = [
+        {
+            name: 'Products',
+            link: '/product/list',
+            activeIndex: 1,
+            selectedIndex: 0
+        }
+    ]
+
+
+
+    const routes = [
+        { name: 'Home', link: '/', activeIndex: 0 },
+        { name: 'Products', link: '/product/list', activeIndex: 1, ariaOwns: anchorEl ? 'simple-menu' : undefined, ariaPopup: anchorEl ? 'true' : undefined, mouseOver: event => handleClick(event) },
+        { name: 'The Revolution', link: '/revolution', activeIndex: 2 },
+        { name: 'About us', link: '/about', activeIndex: 3 },
+        { name: 'Contact us', link: '/contact', activeIndex: 4 },
+        //{ name: 'Sign In', link: '/login', activeIndex: 5 },
+        // { name: 'Sign Up', link: '/register', activeIndex: 6 }
+    ]
+    if (auth.currentUser) {
+        menuOptions.push(
+            {
+                name: 'Create Product',
+                link: '/product/create',
+                activeIndex: 1,
+                selectedIndex: 1
+            }
+        )
+        routes.splice(2, 0, {
+            name: 'Create Product', link: '/product/create', activeIndex: 1
+        })
+    }
+
+    useEffect(() => {
+        [...menuOptions, ...routes].forEach(route => {
+            switch (window.location.pathname) {
+                case `${route.link}`:
+                    if (value !== route.activeIndex) {
+                        setValue(route.activeIndex);
+                        if (route.selectedIndex && route.selectedIndex !== selectedIndex) {
+                            setSelectedIndex(route.selectedIndex)
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        })
+    }, [value, menuOptions, selectedIndex, routes])
 
     const isClickedLogout = () => {
         logout();
@@ -20,68 +115,180 @@ const NavBar = () => {
         console.log('user logout');
         console.log(auth.currentUser);
     }
+    const tabs = (
+        <React.Fragment>
+            <Tabs value={value} onChange={navLinkChangeHandler} className={classes.container} indicatorColor="secondary">
+
+                {routes.map((route, index) => (
+                    <Tab
+                        key={`${route}${index}`}
+                        className={classes.tab}
+                        component={Link}
+                        to={route.link}
+                        label={route.name}
+                        aria-owns={route.ariaOwns}
+                        aria-haspopup={route.ariaPopup}
+                        onMouseOver={route.mouseOver}
+                    />
+                ))}
+                {!auth.currentUser &&
+
+                    <Tab
+                        className={classes.tab}
+                        component={Link}
+                        to='/login'
+                        label='Sign In'
+                    />}
+                {!auth.currentUser &&
+                    <Tab
+                        className={classes.tab}
+                        component={Link}
+                        to='/register'
+                        label='Sign Up'
+                    />
+                }
+                {auth.currentUser &&
+                    <Tab
+                        className={classes.tab}
+                        component={Link}
+                        to='/logout'
+                        label='Logout'
+                        onClick={isClickedLogout}
+                    />}
+
+
+            </Tabs>
+            {/* <Button variant='contained' color='secondary' className={classes.button}>
+                Free Estimate
+            </Button> */}
+
+            <Menu
+                id='simple-menu'
+                anchorEl={anchorEl}
+                open={openMenu}
+                onClose={handleClose}
+                classes={{ paper: classes.menu }}
+                MenuListProps={{ onMouseLeave: handleClose }}
+                elevation={0}
+                style={{ zIndex: 1302 }}
+                keepMounted
+            >
+                {menuOptions.map((option, i) => (
+                    <MenuItem
+                        key={i}
+                        component={Link}
+                        to={option.link}
+                        onClick={(event) => {
+                            handleMenuItemClick(event, i);
+                            setValue(1);
+                            handleClose();
+                        }}
+                        classes={{ root: classes.menuItem }}
+                        selected={i === selectedIndex && value === 1}
+                    >
+                        {option.name}
+                    </MenuItem>
+                ))}
+            </Menu>
+        </React.Fragment>
+    )
+
+    const drawer = (
+        <React.Fragment>
+            <SwipeableDrawer
+                // disableBackdropTransition={!iOS}
+                // disableDiscovery={iOS}
+                open={openDrawer}
+                onClose={() => setOpenDrawer(false)}
+                onOpen={() => setOpenDrawer(true)}
+                classes={{ paper: classes.drawer }}
+            >
+                <div className={classes.toolbarMargin} />
+                <List disablePadding>
+
+                    {routes.map((route, index) => (
+                        <ListItem
+                            key={`${route}${index}`}
+                            divider
+                            button
+                            component={Link}
+                            to={route.link}
+                            selected={value === route.activeIndex}
+                            classes={{ selected: classes.drawerItemSelected }}
+                            onClick={() => { setOpenDrawer(false); setValue(route.activeIndex) }}
+                        >
+                            <ListItemText
+                                className={classes.drawerItem}
+                                disableTypography>{route.name}</ListItemText>
+                        </ListItem>
+                    ))}
+                    {!auth.currentUser &&
+                        <ListItem
+                            classes={{ selected: classes.drawerItemSelected }}
+                            component={Link}
+                            to='/login'
+                            label='Sign In'
+                            onClick={() => { setOpenDrawer(false); setValue(5) }}
+                        >
+                            <ListItemText
+                                className={classes.drawerItem}
+                                disableTypography>Sign In</ListItemText>
+                        </ListItem>}
+                    {!auth.currentUser &&
+                        <ListItem
+                            classes={{ selected: classes.drawerItemSelected }}
+                            component={Link}
+                            to='/register'
+                            label='Sign Up'
+                            onClick={() => { setOpenDrawer(false); setValue(6) }}
+                        >
+                            <ListItemText
+                                className={classes.drawerItem}
+                                disableTypography>Sing Up</ListItemText>
+
+                        </ListItem>}
+                    {auth.currentUser &&
+                        <ListItem
+                            classes={{ selected: classes.drawerItemSelected }}
+                            component={Link}
+                            to='/logout'
+                            label='Logout'
+                            onClick={() => { setOpenDrawer(false); setValue(7) }}
+                        >
+                            <ListItemText
+                                className={classes.drawerItem}
+                                disableTypography>Logout</ListItemText>
+
+                        </ListItem>}
+
+                </List>
+            </SwipeableDrawer>
+            <IconButton className={classes.drawerIconContainer} onClick={() => setOpenDrawer(!openDrawer)}>
+                <MenuIcon className={classes.drawerIcon} />
+            </IconButton>
+        </React.Fragment>
+    )
+
 
     return (
-        <>
-            <AppBar position='sticky' className={classes.appBar} color='inherit'>
-                <Toolbar>
-                    {/* <Typography variant='h6' className={classes.title} color='inherit'>
-                            
-                        </Typography> */}
-                    <Stack spacing={1} direction='row' m={1}>
-                        <Link to='/' variant='secondary' color='primary'>
-                            <ListItem button>
-                                <ListItemIcon>
-                                    <img src={logo} alt='Essence' height="35px" className={classes.image} />
-                                </ListItemIcon>
-                                <ListItemText color='primary'>
-                                    Essence
-                                </ListItemText>
-                            </ListItem>
-                        </Link>
-                        <Link className={classes.link} to='/product/list' variant='secondary' color='primary' >
-                            <ListItem button>
-                                <ListItemText>
-                                    List Products
-                                </ListItemText>
-                            </ListItem>
-                        </Link>
-                        {auth.currentUser &&
-                            <><Link className={classes.link} to='/product/create' variant='secondary' color='primary' >
-                                <ListItem button>
-                                    <ListItemText>
-                                        Create Product
-                                    </ListItemText>
-                                </ListItem>
-                            </Link>
-                                <Link className={classes.link} to='/product/edit' variant='secondary' color='primary' >
-                                    <ListItem button>
-                                        <ListItemText>
-                                            Edit Product
-                                        </ListItemText>
-                                    </ListItem>
-                                </Link>
-                            </>
-                        }
-                    </Stack>
-                    <div className={classes.grow} />
-                    <Stack spacing={3} direction="row">
-                        {!auth.currentUser && <>
-                            <Button component={NavLink} to="/login" variant="contained"> Login </Button>
-                            <Button component={NavLink} to="/register" variant="contained">Register</Button></>}
-                        {auth.currentUser &&
-                            <Button variant="contained" onClick={isClickedLogout}>Logout</Button>}
-                    </Stack>
-                    <div className={classes.button}>
-                        <IconButton area-label="Show cart items" color='inherit'>
-                            <Badge badgeContent={2} color='secondary'>
+        <React.Fragment>
+            <ElevationScroll>
+                <AppBar position='fixed' className={classes.appbar}>
+                    <Toolbar disableGutters>
+                        <Button component={Link} to='/' className={classes.logoContainer} disableRipple onClick={() => setValue(0)}>
+                            <img alt="logo" className={classes.logo} src={logo} />
+                        </Button>
+                        {matches ? drawer : tabs}
+                        <IconButton className={classes.tab} component={Link} to='/product/cart' area-label="Show cart items">
+                            <Badge overlap='rectangular' badgeContent={2}>
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
-                    </div>
-                </Toolbar>
-            </AppBar>
-        </>
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
+            <div className={classes.toolbarMargin} />
+        </React.Fragment>
     )
 }
 
--- essence-shop/src/components/Header/NavBar/navStyle.js
+++ essence-shop/src/components/Header/NavBar/navStyle.js
@@ -2,77 +2,103 @@ import { makeStyles, alpha } from '@material-ui/core/styles';
 
 const drawerWidth = 0;
 
-export default makeStyles((theme) => ({
-    appBar: {
-        padding: "0,0,70px,0",
-        boxShadow: 'none',
-        borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
-        [theme.breakpoints.up('sm')]: {
-            width: `calc(100% - ${drawerWidth}px)`,
-            marginLeft: drawerWidth,
-        },        
+export default makeStyles(theme => ({
+    toolbarMargin: {
+        ...theme.mixins.toolbar,
+        marginBottom: '3em',
+        [theme.breakpoints.down('md')]: {
+            marginBottom: '1.3em',
+        },
+        [theme.breakpoints.down('xs')]: {
+            marginBottom: '1em',
+        },
+
+        textDecoration: 'none'
     },
-    link: {
-        textDecoration: "none",
-        color: theme.palette.text.primary,
+    logo: {
+        height: '7em',
+        [theme.breakpoints.down('md')]: {
+            height: '5em',
+        },
+        [theme.breakpoints.down('xs')]: {
+            height: '4em',
+        },
+
+    },
+    container: {
+        marginLeft: 'auto'
+    },
+    tab: {
+        ...theme.typography.tab,
+        minWidth: 10,
+        marginLeft: "30px",
+        color: theme.palette.primary.secondary,
         "&:hover": {
-            color: theme.palette.text.primary,
-            textDecoration: "none"
+            color: theme.palette.secondary.main,
+            textDecoration: 'none',
+            backgroundColor: 'transparent',
+        },
+        textDecoration: 'none',
+
+    },
+    button: {
+        ...theme.typography.estimate,
+        borderRadius: '50px',
+        marginRight: "25px",
+        marginLeft: "50px",
+        height: '45px',
+
+    },
+    logoContainer: {
+        padding: 0,
+        "&:hover": {
+            backgroundColor: 'transparent',
+
         }
     },
-    title: {
-        flexGrow: 1,
-        alignItems: 'center',
-        display: 'flex',
-        textDecoration: 'none',
+    menu: {
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.primary.secondary,
+        borderRadius: '0px',
     },
-    image: {
-        marginRight: '10px',
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-        [theme.breakpoints.up('sm')]: {
-            display: 'none',
-        },
-    },
-    grow: {
-        flexGrow: 1,
-    },
-    search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: alpha(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: alpha(theme.palette.common.white, 0.05),
-        },
-        marginRight: theme.spacing(2),
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            width: 'auto',
-        },
-    },
-    searchIcon: {
-        padding: theme.spacing(0, 2),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputRoot: {
-        color: 'inherit',
-    },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '20ch',
-        },
-    },
+    menuItem: {
+        ...theme.typography.tab,
+        opacity: 0.7,
+        "&:hover": {
+            opacity: '0.2',
 
-}));
+        }
+    },
+    drawerIconContainer: {
+        marginLeft: 'auto',
+        "&:hover": {
+            backgroundColor: 'transparent',
+
+        }
+    },
+    drawerIcon: {
+        height: '50px',
+        width: '50px',
+        color: 'white'
+    },
+    drawer: {
+        backgroundColor: theme.palette.primary.main,
+    },
+    drawerItem: {
+        ...theme.typography.tab,
+        color: theme.palette.primary.secondary,
+        opacity: 0.7,
+    },
+    drawerItemEstimate: {
+        backgroundColor: theme.palette.secondary.main
+    },
+    drawerItemSelected: {
+        "& .MuiListItemText-root": {
+
+            opacity: 1
+        }
+    },
+    appbar: {
+        zIndex: theme.zIndex.modal + 1
+    }
+}))
